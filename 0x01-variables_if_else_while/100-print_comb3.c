57b5a32c26016b555aa43b721a6a363ede84a8b0
Ismaila Hassan <code.ismailah28@gmail.com>
2022-06-03 22:45:54 -0700

    hotfix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (digit1 = 0; digit1 <= 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 9)
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
@@ -20,5 +20,6 @@ int main(void)
 
 			putchar(',');
 			putchar(' ');
+		}
 	}
 }
