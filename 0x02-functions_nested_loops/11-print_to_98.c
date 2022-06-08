bb53a0d09a5995cf9dddadaf30d06817e6661ceb
Ismaila Hassan <code.ismailah28@gmail.com>
2022-06-07 21:46:38 -0700

    add solution to task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+/**
+* print_to_98 - print numbers from n to 98
+* @n: start number
+* Return: returns void
+*/
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		_putchar (n);
+		_putchar (',');
+		_putchar (' ');
+
+		n++;
+	}
+}
