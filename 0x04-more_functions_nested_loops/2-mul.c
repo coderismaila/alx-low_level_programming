3ecedaeb7b8d0279aef22bc7035396d93fb5560e
Ismaila Hassan <code.ismailah28@gmail.com>
2022-06-09 19:20:50 -0700

    add solution to task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - returns the product of its parameters
+* @a: int first number
+* @b: int second number
+* Return: int
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
