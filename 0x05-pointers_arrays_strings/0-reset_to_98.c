edd1c26d1d8b7880479e2077f2e4f15054190830
Ismaila Hassan <code.ismailah28@gmail.com>
2022-06-12 23:24:45 -0700

    fix betty error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - update the value of a pointer n to 98.
- * @n: pointer whose value will be changed. 
+ * @n: pointer whose value will be changed.
 */
 
 void reset_to_98(int *n)
