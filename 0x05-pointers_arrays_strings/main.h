b388fe8603b5a65a95e9f183bfec1c900bec0c5f
Ismaila Hassan <code.ismailah28@gmail.com>
2022-06-13 20:41:51 -0700

    add solution to task 7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@ void swap_int(int *a, int *b);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char);
