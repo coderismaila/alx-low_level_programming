98286926ac0bc5037cb1daff30f3d142e34c2a37
Ismaila Hassan <code.ismailah28@gmail.com>
2022-06-24 19:13:57 -0700

    add solution to task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: count of arguments in argv
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned short int i = 0;
+
+	while(argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
