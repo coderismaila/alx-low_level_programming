dce1419ed4aa9604e82316ac3c528e738511c0cc
Ismaila Hassan <code.ismailah28@gmail.com>
2022-06-24 20:19:25 -0700

    add solution to task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that adds two numbers.
+ * @argc: count of arguments in argv
+ * @argv: array of arguments passed into the program
+ * Return: 0 if no error, else 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
