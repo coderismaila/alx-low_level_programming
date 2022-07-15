9509d5bd71b8cb2384631b0865eb1c00fe367796
Ismaila Hassan <code.ismailah28@gmail.com>
2022-07-15 08:33:20 -0700

    fix error

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,10 +4,11 @@
  * free_list - function that frees a list_t list.
  * @head: head of linked list
  */
+
 void free_list(list_t *head)
 {
 	list_t *cur;
-	list_t *next;
+	list_t *nxt;
 
 	cur = head;
 	while (cur != NULL)
