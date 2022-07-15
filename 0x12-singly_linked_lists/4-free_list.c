508325cbc2b7ceffcc837b2a31c94575a2be44ab
Ismaila Hassan <code.ismailah28@gmail.com>
2022-07-15 08:03:25 -0700

    add solution to task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: head of linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *cur;
+	list_t *next;
+
+	cur = head;
+	while (cur != NULL)
+	{
+		nxt = cur->next;
+		free(cur->str);
+		free(cur);
+		cur = nxt;
+	}
+}
